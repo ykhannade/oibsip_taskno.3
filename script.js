07d4eb4dce3145b77d0da6511539415d02803dc4
ykhannade <96043468+ykhannade@users.noreply.github.com>
2022-03-04 13:55:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let celsius =document.getElementById("celsius");
+let fahrenheit=document.getElementById("fahrenheit")
+
+function celTofar(){
+    let output = (parseFloat(celsius.value) * 9/5) + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+    
+
+}
+
+function farTocel(){
+    let output =(parseFloat(fahrenheit.value) -32)* 5/9;
+    celsius.value=parseFloat( output.toFixed(2));
+    console.log(output);
+}
